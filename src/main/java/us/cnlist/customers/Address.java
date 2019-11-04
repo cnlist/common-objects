ef25b23122d0dcb2a7f58d2876e9ff534912cc90
Sergei W. Adonev <admin@cnlist.us>
2019-11-04 23:08:42 +0500

    Address class

---
+++ src/main/java/us/cnlist/customers/Address.java
@@ -0,0 +1,84 @@
+package us.cnlist.customers;
+
+import java.io.Serializable;
+
+public class Address implements Serializable {
+
+    private String city;
+    private String country;
+    private String province;
+    private String houseNumber;
+    private String postBox;
+    private String apartment;
+    private String street;
+    private String zipCode;
+
+
+    public Address(){
+
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getProvince() {
+        return province;
+    }
+
+    public void setProvince(String province) {
+        this.province = province;
+    }
+
+    public String getHouseNumber() {
+        return houseNumber;
+    }
+
+    public void setHouseNumber(String houseNumber) {
+        this.houseNumber = houseNumber;
+    }
+
+    public String getPostBox() {
+        return postBox;
+    }
+
+    public void setPostBox(String postBox) {
+        this.postBox = postBox;
+    }
+
+    public String getApartment() {
+        return apartment;
+    }
+
+    public void setApartment(String apartment) {
+        this.apartment = apartment;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public String getZipCode() {
+        return zipCode;
+    }
+
+    public void setZipCode(String zipCode) {
+        this.zipCode = zipCode;
+    }
+}
