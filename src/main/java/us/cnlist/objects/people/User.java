0b33c86c0a452636d2fab2ce7ca6aa57f8d5d037
sergei w adonev <admin@cnlist.us>
2020-05-03 21:54:15 +0300

    CSTM-11 Fix user ojbect

--- src/main/java/us/cnlist/objects/people/User.java
+++ src/main/java/us/cnlist/objects/people/User.java
@@ -2,13 +2,26 @@ package us.cnlist.objects.people;
 
 import lombok.Data;
 
+import javax.persistence.*;
 import java.io.Serializable;
+import java.util.Date;
 
 @Data
+@Entity
+@Table(name = "system_user")
 public class User implements Serializable {
 
+    @Id
+    @SequenceGenerator(name = "seq_user", allocationSize = 1, sequenceName = "seq_sysuser")
+    @GeneratedValue(generator = "seq_user")
+    private Long id;
+    @Column(name = "user_login")
     private String login;
+    @Column(name = "user_password")
     private String password;
-    private UserProfile profile;
+    private boolean enabled;
+    @Column(name = "registration_date")
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date registrationDate;
 
 }
--- src/main/java/us/cnlist/objects/responses/UserRegistrationRS.java
+++ src/main/java/us/cnlist/objects/responses/UserRegistrationRS.java
@@ -5,5 +5,8 @@ import lombok.Data;
 @Data
 public class UserRegistrationRS {
     private Long userId;
+    private String errorMessage;
+    private ResponseType responseType;
+    private ErrorType errorType;
 
 }
